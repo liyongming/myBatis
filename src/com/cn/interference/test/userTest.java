ae849b8f410b330c1a4adba6e141d61419088fb7
liyongming <2207265761@qq.com>
2017-11-13 13:39:32 +0800

    接口开发

--- src/com/cn/interference/test/userTest.java
+++ src/com/cn/interference/test/userTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import com.cn.interference.tb_userInterference;
 import com.cn.po.tb_order;
 import com.cn.po.tb_user;
 import com.cn.util.sqlSessionFactory;
@@ -29,6 +30,20 @@ public class userTest {
 		SqlSession session = sqlSessionFactory.getSqlSession();
 		System.out.println(session);
 		tb_user user = session.selectOne("com.cn.interference.userMapper.selectById","2");
+		List<tb_order> orders = user.getOrders();
+		for(int i = 0;i<orders.size();i++){
+			System.out.println(orders.get(i).getCode());
+		}
+	}
+	/**
+	 * 使用接口开发，需要使得接口名字，mapper.xml中的namespace中的名字，是一致的。
+	 * 同时调用的方法，接口中的方法，和mapper.xml中的方法是一致的
+	 */
+	@Test
+	public void selectStudentByInterfence(){
+		SqlSession sqlSession = sqlSessionFactory.getSqlSession();
+		tb_userInterference mapper = sqlSession.getMapper(tb_userInterference.class);
+		tb_user user = mapper.selectById("2");
 		List<tb_order> orders = user.getOrders();
 		for(int i = 0;i<orders.size();i++){
 			System.out.println(orders.get(i).getCode());
